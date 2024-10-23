dfea0fc3e0bcc0d24892a31f3cbf9d7cfc071595
Folksy <jespeyw26@gmail.com>
2024-10-23 11:22:02 -0400

    added Java assignment 1

---
+++ JavaAssignment1/assignment.js
@@ -0,0 +1,17 @@
+let a = "A string";
+let b = 4;
+let c = true;
+let d;
+let e = null;
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
